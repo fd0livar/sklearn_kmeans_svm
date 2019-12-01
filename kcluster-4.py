3c08cb5ead9307e346444da32d87df6387734a93
fd0livar <fdolivar@gmail.com>
2019-12-01 20:38:33 +0100

    new code

--- kcluster-4.py
+++ kcluster-4.py
@@ -3,3 +3,19 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.cluster import KMeans
+
+X, y = make_blobs (n_samples=300, centers=4, cluster_std=0.60, random_state=0)
+plt.figure()
+plt.scatter(X[:,0], X[:,1])
+
+wcss = []
+for i in range(1, 15):
+    kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+plt.figure()
+plt.plot(range(1, 15), wcss, '*-')
+plt.title('Elbow Method')
+plt.xlabel('Number of clusters')
+plt.ylabel('WCSS')
+plt.show()
