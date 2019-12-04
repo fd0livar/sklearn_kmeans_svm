c8ad643b80e46aa10fff4a84709c045cccdd3e2d
fd0livar <fdolivar@gmail.com>
2019-12-04 14:45:28 +0100

    printing values for centroids and labels

--- kcluster2nd.py
+++ kcluster2nd.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from matplotlib import style
 style.use("ggplot")
 
@@ -9,10 +9,10 @@ style.use("ggplot")
 
 X = np.array([[1, 2], [5, 8], [1.5, 1.8], [8, 8], [1, 0.6], [9, 11]])
 kmeans = KMeans(n_clusters=2)
-kmeans.fit(X) #we fit the data to learn from it
+kmeans.fit(X)  # we fit the data to learn from it
 
-centroids = kmeans.cluster_centers_ #we grab the values found from the fit
+centroids = kmeans.cluster_centers_  # we grab the values found from the fit
 labels = kmeans.labels_
 
-print(centroids)
-print(labels)
+print(np.size(centroids), centroids)
+print(np.size(labels), labels)
