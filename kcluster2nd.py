429a883e02830a7c4337131096d82e08c909e88f
fd0livar <fdolivar@gmail.com>
2019-12-04 14:33:33 +0100

    initializing kmeans for 2 clusters

--- kcluster2nd.py
+++ kcluster2nd.py
@@ -1,11 +1,18 @@
-# import numpy as np
-# from sklearn.cluster import KMeans
+import numpy as np
+from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from matplotlib import style
 style.use("ggplot")
 
-x = [1, 5, 1.5, 8, 1, 9]
-y = [2, 8, 1.8, 8, 0.6, 11]
+# x = [1, 5, 1.5, 8, 1, 9]
+# y = [2, 8, 1.8, 8, 0.6, 11]
 
-plt.scatter(x, y)
-plt.show()
+X = np.array([[1, 2], [5, 8], [1.5, 1.8], [8, 8], [1, 0.6], [9, 11]])
+kmeans = KMeans(n_clusters=2)
+kmeans.fit(X) #we fit the data to learn from it
+
+centroids = kmeans.cluster_centers_ #we grab the values found from the fit
+labels = kmeans.labels_
+
+print(centroids)
+print(labels)
