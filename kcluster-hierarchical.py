51b2cf5d554829f8205e9b470a290b6e85bfff8c
fd0livar <fdolivar@gmail.com>
2019-12-04 18:33:54 +0100

    2d and 3d for hierarchical clustering

--- kcluster-hierarchical.py
+++ kcluster-hierarchical.py
@@ -7,3 +7,25 @@ style.use("ggplot")
 
 centers = [[1, 1], [5, 5], [3, 10]]
 X, _ = make_blobs(n_samples=500, centers=centers, cluster_std=1)
+
+# plt.scatter(X[:, 0], X[:, 1])
+# plt.show()
+
+ms = MeanShift()
+ms.fit(X)
+labels = ms.labels_
+cluster_centers = ms.cluster_centers_
+
+print(cluster_centers)
+n_clusters_ = len(np.unique(labels))
+print('Number of estimated clusters: ', n_clusters_)
+
+colors = 10*['r.', 'g.', 'b.', 'c.', 'k.', 'y.', 'm.']
+
+for i in range(len(X)):
+    plt.plot(X[i][0], X[i][1], colors[labels[i]], markersize=5)
+
+plt.scatter(cluster_centers[:, 0], cluster_centers[:, 1],
+            marker='x', color='k', s=100, linewidths=5, zorder=10)
+
+plt.show()
