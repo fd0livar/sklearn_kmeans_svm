08501ace39d69993380e689143e43db800979f52
fd0livar <fdolivar@gmail.com>
2019-12-05 19:46:12 +0100

    new code: it creates a csv with the data gathered

--- project_st_data.py
+++ project_st_data.py
@@ -9,7 +9,7 @@ path = "C:/Users/fdoli/github/ScikitLearn/intraQuarter"
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path + '/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
-    # print(stock_list)
+    df = pd.DataFrame(columns=['Date', 'Unix', 'Ticker', 'DE Ratio'])
     for each_dir in stock_list[1:]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("\\")[1]
@@ -17,15 +17,16 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(date_stamp.timetuple())
-                # print(date_stamp, unix_time)
                 full_file_path = each_dir + '/' + file
-                print(full_file_path)
                 source = open(full_file_path, 'r').read()
-                # print(source)
-                value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
-                print(ticker + ': ' + value)
-
-
+                try:
+                    value = float(source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date': date_stamp, 'Unix': unix_time, 'Ticker': ticker, 'DE Ratio': value, },
+                                   ignore_index=True)
+                except Exception as e:
+                    pass
+    save = gather.replace(' ', '').replace(')', '').replace('(', '').replace('/', '')+'.csv'
+    df.to_csv(path + '/project/' + save)  # saving csv file inside git-ignored folder
 
 
 Key_Stats()
