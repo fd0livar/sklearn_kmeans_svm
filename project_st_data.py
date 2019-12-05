966c0d2db6e11fcca26978e4c52c2dad5baa763b
fd0livar <fdolivar@gmail.com>
2019-12-05 23:14:05 +0100

    saving columns related to stock changes

--- project_st_data.py
+++ project_st_data.py
@@ -9,12 +9,25 @@ path = "C:/Users/fdoli/github/ScikitLearn/intraQuarter"
 def Key_Stats(gather="Total Debt/Equity (mrq)"):
     statspath = path + '/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
-    df = pd.DataFrame(columns=['Date', 'Unix', 'Ticker', 'DE Ratio', 'Price', 'SP500'])
+    df = pd.DataFrame(columns=['Date',
+                               'Unix',
+                               'Ticker',
+                               'DE Ratio',
+                               'Price',
+                               'stock_p_change',
+                               'SP500',
+                               'SP500_p_change'])
     sp500_df = pd.read_csv(path + '/project/' + 'YAHOO-INDEX_GSPC.csv')
+    ticker_list = []
 
     for each_dir in stock_list[1:25]:
         each_file = os.listdir(each_dir)
         ticker = each_dir.split("\\")[1]
+        ticker_list.append(ticker)
+
+        starting_stock_value = False
+        starting_sp500_value = False
+
         if len(each_file) > 0:  # bc some folders are empty
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
@@ -36,13 +49,22 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
 
                     stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
 
+                    if not starting_stock_value:
+                        starting_stock_value = stock_price
+                    if not starting_sp500_value:
+                        starting_sp500_value = sp500_value
+
+                    stock_p_change = ((stock_price - starting_stock_value) / starting_stock_value) * 100
+                    sp500_p_change = ((sp500_value - starting_sp500_value) / starting_sp500_value) * 100
+
                     df = df.append({'Date': date_stamp,
                                     'Unix': unix_time,
                                     'Ticker': ticker,
                                     'DE Ratio': value,
                                     'Price': stock_price,
-                                    'SP500': sp500_value}, ignore_index=True)
-
+                                    'stock_p_change': stock_p_change,
+                                    'SP500': sp500_value,
+                                    'sp500_p_change': sp500_p_change}, ignore_index=True)
                 except Exception as e:
                     pass
 
