ba8777653248a5eaace1874a7621f8ad8089a1d4
fd0livar <fdolivar@gmail.com>
2019-12-05 19:10:38 +0100

    we can get the values using -splitting- of the html source code

--- project_st_data.py
+++ project_st_data.py
@@ -17,7 +17,15 @@ def Key_Stats(gather="Total Debt/Equity (mrq)"):
             for file in each_file:
                 date_stamp = datetime.strptime(file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(date_stamp.timetuple())
-                print(date_stamp, unix_time)
+                # print(date_stamp, unix_time)
+                full_file_path = each_dir + '/' + file
+                print(full_file_path)
+                source = open(full_file_path, 'r').read()
+                # print(source)
+                value = source.split(gather + ':</td><td class="yfnc_tabledata1">')[1].split('</td>')[0]
+                print(ticker + ': ' + value)
+
+
 
 
 Key_Stats()
