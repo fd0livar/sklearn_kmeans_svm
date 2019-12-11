48d87bb945b1c09131d5a22b0bb8c7360c89122a
fd0livar <fdolivar@gmail.com>
2019-12-11 23:05:29 +0100

    we train and test with more features

--- 1st_build_n_analysis.py
+++ 1st_build_n_analysis.py
@@ -1,43 +1,83 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn import svm
+import time
+from sklearn import svm, preprocessing
 from matplotlib import style
 style.use('ggplot')
 
 path = "C:/Users/fdoli/github/ScikitLearn/intraQuarter/project/"
-def build_data_set(features=['DE Ratio', 'Trailing P/E' ]):
 
+FEATURES = ['DE Ratio',
+           'Trailing P/E',
+           'Price/Sales',
+           'Price/Book',
+           'Profit Margin',
+           'Operating Margin',
+           'Return on Assets',
+           'Return on Equity',
+           'Revenue Per Share',
+           'Market Cap',
+           'Enterprise Value',
+           'Forward P/E',
+           'PEG Ratio',
+           'Enterprise Value/Revenue',
+           'Enterprise Value/EBITDA',
+           'Revenue',
+           'Gross Profit',
+           'EBITDA',
+           'Net Income Avl to Common ',
+           'Diluted EPS',
+           'Earnings Growth',
+           'Revenue Growth',
+           'Total Cash',
+           'Total Cash Per Share',
+           'Total Debt',
+           'Current Ratio',
+           'Book Value Per Share',
+           'Cash Flow',
+           'Beta',
+           'Held by Insiders',
+           'Held by Institutions',
+           'Shares Short (as of',
+           'Short Ratio',
+           'Short % of Float',
+           'Shares Short (prior ']
+
+def build_data_set():
     data_df = pd.read_csv(path + 'key_stats.csv')
-    data_df = data_df[:100]
+    # data_df = data_df[:100]
 
-    X = np.array(data_df[features].values)
+    X = np.array(data_df[FEATURES].values)
 
     y = (data_df['Status']
          .replace('underperform', 0)
          .replace('outperform', 1)
          .values.tolist())
 
+    X = preprocessing.scale(X)
+
     return X, y
 
+
 def analysis():
+    test_size = 1000
     X, y = build_data_set()
+    print(len(X))
 
     clf = svm.SVC(kernel='linear', C=1.0)
-    clf.fit(X, y)
+    clf.fit(X[:-test_size], y[:-test_size])
+    correct_count = 0
 
-    w = clf.coef_[0]
-    a = -w[0] / w[1]
-    xx = np.linspace(min(X[:, 0]), max(X[:, 0]))
-    yy = a * xx - clf.intercept_[0] / w[1]
+    for xx in range(1, test_size+1):
+        # print(clf.predict(X[-xx].reshape(1, -1))[0])
 
-    h0 = plt.plot(xx, yy, 'k-', label='non weighted')
+        if clf.predict(X[-xx].reshape(1, -1))[0] == y[-xx]:
+            correct_count += 1
 
-    plt.scatter(X[:, 0], X[:, 1], c=y)
-    plt.ylabel('Trailing P/E')
-    plt.xlabel('DE Ratio')
-    plt.legend()
+    # correct_count = np.sum(clf.predict(X[-test_size:].reshape(1, -1)) == y[-test_size:])
 
-    plt.show()
+    print('Accuracy: ', (correct_count/test_size) * 100.00)
+
 
 analysis()
