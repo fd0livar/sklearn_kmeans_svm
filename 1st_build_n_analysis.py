a5e707474bdefd80efc99822e1cb54c407dc89ee
fd0livar <fdolivar@gmail.com>
2019-12-12 12:12:26 +0100

    indentation adjustments

--- 1st_build_n_analysis.py
+++ 1st_build_n_analysis.py
@@ -1,48 +1,49 @@
 import numpy as np
 # import matplotlib.pyplot as plt
 import pandas as pd
-import time
 from sklearn import svm, preprocessing
 from matplotlib import style
+
 style.use('ggplot')
 
 path = "C:/Users/fdoli/github/ScikitLearn/intraQuarter/project/"
 
 FEATURES = ['DE Ratio',
-           'Trailing P/E',
-           'Price/Sales',
-           'Price/Book',
-           'Profit Margin',
-           'Operating Margin',
-           'Return on Assets',
-           'Return on Equity',
-           'Revenue Per Share',
-           'Market Cap',
-           'Enterprise Value',
-           'Forward P/E',
-           'PEG Ratio',
-           'Enterprise Value/Revenue',
-           'Enterprise Value/EBITDA',
-           'Revenue',
-           'Gross Profit',
-           'EBITDA',
-           'Net Income Avl to Common ',
-           'Diluted EPS',
-           'Earnings Growth',
-           'Revenue Growth',
-           'Total Cash',
-           'Total Cash Per Share',
-           'Total Debt',
-           'Current Ratio',
-           'Book Value Per Share',
-           'Cash Flow',
-           'Beta',
-           'Held by Insiders',
-           'Held by Institutions',
-           'Shares Short (as of',
-           'Short Ratio',
-           'Short % of Float',
-           'Shares Short (prior ']
+            'Trailing P/E',
+            'Price/Sales',
+            'Price/Book',
+            'Profit Margin',
+            'Operating Margin',
+            'Return on Assets',
+            'Return on Equity',
+            'Revenue Per Share',
+            'Market Cap',
+            'Enterprise Value',
+            'Forward P/E',
+            'PEG Ratio',
+            'Enterprise Value/Revenue',
+            'Enterprise Value/EBITDA',
+            'Revenue',
+            'Gross Profit',
+            'EBITDA',
+            'Net Income Avl to Common ',
+            'Diluted EPS',
+            'Earnings Growth',
+            'Revenue Growth',
+            'Total Cash',
+            'Total Cash Per Share',
+            'Total Debt',
+            'Current Ratio',
+            'Book Value Per Share',
+            'Cash Flow',
+            'Beta',
+            'Held by Insiders',
+            'Held by Institutions',
+            'Shares Short (as of',
+            'Short Ratio',
+            'Short % of Float',
+            'Shares Short (prior ']
+
 
 def build_data_set():
     data_df = pd.read_csv(path + 'key_stats.csv')
@@ -70,7 +71,7 @@ def analysis():
     clf.fit(X[:-test_size], y[:-test_size])
     correct_count = 0
 
-    for xx in range(1, test_size+1):
+    for xx in range(1, test_size + 1):
         # print(clf.predict(X[-xx].reshape(1, -1))[0])
 
         if clf.predict(X[-xx].reshape(1, -1))[0] == y[-xx]:
@@ -78,7 +79,7 @@ def analysis():
 
     # correct_count = np.sum(clf.predict(X[-test_size:].reshape(1, -1)) == y[-test_size:])
 
-    print('Accuracy: ', (correct_count/test_size) * 100.00)
+    print('Accuracy: ', (correct_count / test_size) * 100.00)
 
 
 analysis()
