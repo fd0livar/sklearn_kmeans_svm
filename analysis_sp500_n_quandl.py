18f9f2b28fb4b8555f5ba63ae16c83f3536342ad
fd0livar <fdolivar@gmail.com>
2019-12-16 13:51:30 +0100

    code comparing investment in sp500 with using this strategy

--- analysis_sp500_n_quandl.py
+++ analysis_sp500_n_quandl.py
@@ -59,13 +59,19 @@ def build_data_set():
          .values.tolist())
 
     X = preprocessing.scale(X)
+    Z = np.array(data_df[["stock_p_change", "sp500_p_change"]])
 
-    return X, y
+    return X, y, Z
 
 
 def analysis():
     test_size = 1000
-    X, y = build_data_set()
+    invest_amount = 10000
+    total_invests = 0
+    if_market = 0
+    if_strat = 0
+
+    X, y, Z = build_data_set()
     print(len(X))
 
     clf = svm.SVC(kernel='linear', C=1.0)
@@ -77,7 +83,26 @@ def analysis():
         if clf.predict(X[-xx].reshape(1, -1))[0] == y[-xx]:
             correct_count += 1
 
-    print('Accuracy: ', (correct_count / test_size) * 100.00)
+        if clf.predict(X[-xx].reshape(1, -1))[0] == 1:
+            invest_return = invest_amount + (invest_amount * (Z[-xx][0] / 100))
+            market_return = invest_amount + (invest_amount * (Z[-xx][1] / 100))
+            total_invests += 1
+            if_market += market_return
+            if_strat += invest_return
 
+    print('Accuracy: ', (correct_count / test_size) * 100.00)
+    print("Total Trades:", total_invests)
+    print("Ending with Strategy:", if_strat)
+    print("Ending with Market:", if_market)
+
+    compared = ((if_strat - if_market) / if_market) * 100.0
+    do_nothing = total_invests * invest_amount
+
+    avg_market = ((if_market - do_nothing) / do_nothing) * 100.0
+    avg_strat = ((if_strat - do_nothing) / do_nothing) * 100.0
+
+    print("Compared to market, we earn", str(compared) + "% more")
+    print("Average investment return:", str(avg_strat) + "%")
+    print("Average market return:", str(avg_market) + "%")
 
 analysis()
